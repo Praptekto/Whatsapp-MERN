5cc43451bdd4051be2b979d3cfb2465abd3506e7
PraptektoGIT <praptekto@gmail.com>
2021-03-01 15:24:15 +0700

    Initialize project

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyADrJV_yMYg7Ms-HrJgY9KMkMoIQz1y6N8",
+    authDomain: "whatsapp-mern-ad726.firebaseapp.com",
+    projectId: "whatsapp-mern-ad726",
+    storageBucket: "whatsapp-mern-ad726.appspot.com",
+    messagingSenderId: "123941963433",
+    appId: "1:123941963433:web:34a59f7b98667b7ceb0bb8",
+    measurementId: "G-5G79H5GPNX"
+  };
