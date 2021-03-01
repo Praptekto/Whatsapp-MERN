84429859f8700e2bc8b5a4762c546a61132691c1
PraptektoGIT <praptekto@gmail.com>
2021-03-02 01:41:18 +0700

    Front-end Done

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,91 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, InsertEmoticon, MoreVert, SearchOutlined } from '@material-ui/icons';
 import React from 'react'
 import './Chat.css';
+import MicIcon from '@material-ui/icons/Mic';
+
 function Chat() {
     return (
         <div className="chat">
-            Chat Section
+
+            <div className="chat__header">
+                <Avatar/>
+                <div className="chat__headerInfo">
+                    <h3>Room Name</h3>
+                    <p>Something.............</p>
+                </div>
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined/>
+
+                    </IconButton>
+                        
+
+                    <IconButton>
+                        <AttachFile/>                        
+                    </IconButton>
+
+                    <IconButton>
+                        <MoreVert/>    
+                    </IconButton>
+                </div>
+            </div>
+            <div className="chat__body">
+                <p className="chat__message">
+                    <span className="chat__name">
+                        Praptekto
+                    </span>
+                    Ini adalah isi pesan
+                    
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+
+
+                </p>
+
+                <p className="chat__message chat__receiver">
+                    <span className="chat__name">
+                        Praptekto
+                    </span>
+                    Ini adalah isi pesan
+                    
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+
+
+                </p>
+
+                <p className="chat__message">
+                    <span className="chat__name">
+                        Praptekto
+                    </span>
+                    Ini adalah isi pesan
+                    
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+
+
+                </p>
+            </div>
+            <div className="chat__footer">
+                <InsertEmoticon/>
+                <form>
+                    <input 
+                    placeholder="Type A Message"                    
+                    type="text"/>
+                    <button type ="submit">
+                        Send A message
+                    </button>
+                    
+
+                </form>
+                <MicIcon/>
+
+            </div>
+
             
         </div>
     )
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React from 'react';
 import './Sidebar.css';
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import { IconButton,Avatar } from '@material-ui/core';
+import ChatIcon from '@material-ui/icons/Chat';
+import { MoreVert, SearchOutlined } from '@material-ui/icons';
+import SidebarChat from './SidebarChat';
+
 function Sidebar() {
 
     return (
         <div className="sidebar">
-            Sidebar
+            <div className="sidebar__header">
+                <Avatar src=""/>
+                <div className="sidebar__headerRight">
+
+                    <IconButton>
+                        <DonutLargeIcon/>
+                    </IconButton>
+
+                    <IconButton>
+                        <ChatIcon/>
+                    </IconButton>
+
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+                    
+                </div>
+
+            </div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined/>
+                    <input placeholder="Search or start new chat"/>
+
+                </div>
+            </div>
+
+            <div className="sidebar__chats">
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>            
+            </div>
+
             
         </div>
     )
